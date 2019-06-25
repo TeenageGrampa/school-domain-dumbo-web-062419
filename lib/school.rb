592b1479a0f72b7e71a8fe425eb4fe66b5ad8782
Daniel Voigt <danielhvoigt@gmail.com>
2019-06-25 21:06:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
-# code here!
+class School
+
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def roster
+		@roster
+	end
+
+	def add_student(name, grade)
+		@name = name
+		@grade = grade
+		if roster.include?(grade) == false
+			@roster[grade] << []
+		end
+		roster[grade] << name
+	end
+
+	def grade(num)
+		@roster[num]
+	end
+
+	def sort
+		@roster.each do |name, grade|
+			@roster[grade] = name.sort
+		end
+	end
